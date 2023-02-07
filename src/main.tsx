8fb87d05788a53e4b7e324b1c5c51985bda4bf12
phlima3 <psantoslima86@gmail.com>
2023-02-06 22:38:06 -0300

    fix: routes

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,12 @@ import { RouterProvider, BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Timeline } from "./pages/Timeline/Timeline";
 import { Status } from "./pages/Status/Status";
+import { Default } from "./layouts/Default";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/">
+      <Route path="/" element={<Default />}>
         <Route index element={<Timeline />} />
       </Route>
       <Route path="/status">
